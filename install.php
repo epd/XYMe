db1dd8a9d734ad9402a130999979549ab3842d5b
Evan Dudla <me@evandudla.com>
2012-04-15 17:00:19 -0400

    Code organization and added install confirmation

--- install.php
+++ install.php
@@ -1,52 +1,101 @@
 <?php
-require 'database.php';
+/**
+ * @file
+ * Install script.
+ *
+ * Use this to setup the schema for Users, Groups, and Permissions.
+ */
+require __DIR__ . '/lib/database.php';
 
 class Install {
-  function setup() {
+
+  /**
+   * Implements setup().
+   */
+  static public function setup() {
+    // Connect to the database
     $dbconn = self::connect();
+
+    // Create our tables from schema
     self::createTables($dbconn);
+
+    // Create administrative account
     self::createGroups($dbconn);
     self::addAdmin($dbconn);
   }
 
-  private function connect() {
+  /**
+   * Implements connect().
+   */
+  static private function connect() {
     return Database::connect();
   }
 
-  private function createTables($dbconn) {
+  /**
+   * Implements createTables().
+   *
+   * @param $dbconn
+   * The database connection resource.
+   */
+  static private function createTables($dbconn) {
+
+    // Create our groups table
     $groups_stmt = $dbconn->prepare('create table groups (group_id int(11) not
       null auto_increment primary key, name varchar(255))');
+
+    // Creat our users table
     $users_stmt = $dbconn->prepare('create table users (user_id int(11) not null
       auto_increment primary key, group_id int(11), username varchar(255),
       password varchar(255), foreign key (group_id) references groups(group_id))
     ');
+
+    // Create our roles table
     $roles_stmt = $dbconn->prepare('create table roles (role_id int(11) not
       null auto_increment primary key, name varchar(255))');
+
+    // Create table that holds relation between groups and roles
     $gr_map_stmt = $dbconn->prepare('create table group_role_maps (group_id
       int(11), role_id int(11), foreign key (group_id) references groups
       (group_id), foreign key (role_id) references roles(role_id), primary key
       (group_id, role_id))');
 
+    // Go ahead and execute our statements
     $groups_stmt->execute();
     $users_stmt->execute();
     $roles_stmt->execute();
     $gr_map_stmt->execute();
   }
 
-  private function createGroups($dbconn) {
+  /**
+   * Implements createGroups().
+   *
+   * @param $dbconn
+   * The database connection resource.
+   */
+  static private function createGroups($dbconn) {
     $groups_stmt = $dbconn->prepare('insert into groups values(null, :name)');
 
     $groups_stmt->execute(array(':name' => 'admin'));
     $groups_stmt->execute(array(':name' => 'user'));
   }
 
-  private function addAdmin($dbconn) {
-    require 'config.php';
+  /**
+   * Implements addAdmin().
+   *
+   * @param $dbconn
+   * The database connection resource.
+   */
+  static private function addAdmin($dbconn) {
+    require __DIR__ . '/config.php';
+
+    // Admin account credentials
     $user = 'admin';
     $pass = 'admin';
 
+    // Salt the password
     $salted = sha1($config['salt'] . $pass);
 
+    // Insert our admin account
     $admin_stmt = $dbconn->prepare('insert into users values(null, :groupid,
       :username, :password)');
     $admin_stmt->execute(array(
@@ -57,4 +106,18 @@ class Install {
   }
 }
 
+// Perform the install
 Install::setup();
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Installation</title>
+  </head>
+  <body>
+    <h1>Installation Complete</h1>
+    <p><a href="/">Back to home</a></p>
+  </body>
+</html>
--- lib/database.php
+++ lib/database.php
@@ -1,10 +1,23 @@
 <?php
+/** 
+ * @file
+ * Establish database connection via PDO
+ */
+
 class Database {
+
+  /**
+   * Implements connect().
+   */
   public static function connect() {
-    require 'config.php';
+    require __DIR__ . '/../config.php';
+
+    // Try to connect via PDO to MySQL database
     try {
       return new PDO('mysql:host=localhost;dbname='.$config['db'], $config['user'], $config['pass']);
     }
+
+    // Else, error occurred
     catch (Exception $e) {
       echo "Error: " . $e->getMessage();
     }
